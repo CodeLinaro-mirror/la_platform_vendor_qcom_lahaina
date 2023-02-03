55936594284c0250edf5223d6e747deece23e40f
Xianshan Qu <quic_xianqu@quicinc.com>
2023-02-03 16:45:33 +0800

    lahaina: inherit 2GB dalvik heap settings
    Change-Id: Id4b09997a6c4b41ab2f9a5cc124a1a977344feb3

--- lahaina.mk
+++ lahaina.mk
@@ -139,7 +139,7 @@ $(call inherit-product, build/make/target/product/go_defaults_common.mk)
 
 TARGET_DEFINES_DALVIK_HEAP := true
 $(call inherit-product, device/qcom/vendor-common/common64.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # beluga settings
 PRODUCT_PROPERTY_OVERRIDES += \
