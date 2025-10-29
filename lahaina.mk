f4894c0546f009b8af2fd7d1db788711dd5946d8
Ashok Gandla <c_agandl@qti.qualcomm.com>
2025-10-29 21:38:20 +0530

    lahaina: Bring up changes for lahaina with kernel 6.12
    Change-Id: I7f299a52ce348e66896e36b77b4bd112af428341

--- lahaina.mk
+++ lahaina.mk
@@ -1,16 +1,25 @@
 TARGET_BOARD_PLATFORM := lahaina
+TARGET_BOARD_FAMILY := f-lahaina
+
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 
 BUILD_BROKEN_DUP_RULES := true
-TEMPORARY_DISABLE_PATH_RESTRICTIONS := true
 
 RELAX_USES_LIBRARY_CHECK := true
+
+#Flag to Enable 64 bit only configuration
+TARGET_SUPPORTS_64_BIT_ONLY := true
 
 # Default Android A/B configuration
 ENABLE_AB ?= true
 
+# Enable virtual A/B
 ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+# Enable virtual A/B compression
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/vabc_features.mk)
+PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
+PRODUCT_VENDOR_PROPERTIES += ro.virtual_ab.compression.threads=true
 
 # Enable debugfs restrictions
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
@@ -18,7 +27,33 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 #Enable vm support
 TARGET_ENABLE_VM_SUPPORT := true
 
+#Enable SMCI-Listeners.
+TARGET_ENABLE_SMCI_SYSLISTENER := true
+
+TARGET_NO_QSH_WWAN_SUPPORT := true
+
+# true: earlycon and console enabled
+# false: console explicitly disabled
+# <empty>: default from kernel
+TARGET_CONSOLE_ENABLED ?=
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Set GRF/Vendor freeze properties
+BOARD_SHIPPING_API_LEVEL := 202504
+
+#Align ELF segment of binaries to 16k
+PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 16384
+
+#Enable AOSP to be page size agnostic
+PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
+
+# Set SoC manufacturer property
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.soc.manufacturer=QTI
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.board.family=f-lahaina
 
 # For QSSI builds, we should skip building the system image. Instead we build the
 # "non-system" images (that we support).
@@ -26,6 +61,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 PRODUCT_BUILD_SYSTEM_IMAGE := false
 PRODUCT_BUILD_SYSTEM_OTHER_IMAGE := false
 PRODUCT_BUILD_VENDOR_IMAGE := true
+PRODUCT_BUILD_VENDOR_DLKM_IMAGE := true
 PRODUCT_BUILD_PRODUCT_IMAGE := false
 PRODUCT_BUILD_SYSTEM_EXT_IMAGE := false
 PRODUCT_BUILD_ODM_IMAGE := false
@@ -35,6 +71,7 @@ else
 PRODUCT_BUILD_CACHE_IMAGE := true
 endif
 PRODUCT_BUILD_RAMDISK_IMAGE := true
+PRODUCT_BUILD_RECOVERY_IMAGE := true
 PRODUCT_BUILD_USERDATA_IMAGE := true
 
 # Also, since we're going to skip building the system image, we also skip
@@ -44,6 +81,16 @@ TARGET_SKIP_OTA_PACKAGE := true
 # Enable AVB 2.0
 BOARD_AVB_ENABLE := true
 
+# Disable verified boot checks in abl if AVB is not enabled
+ifeq ($(BOARD_AVB_ENABLE), true)
+BOARD_ABL_SIMPLE := false
+else
+BOARD_ABL_SIMPLE := true
+endif
+
+# Set SYSTEMEXT_SEPARATE_PARTITION_ENABLE if was not already set (set earlier via build.sh).
+SYSTEMEXT_SEPARATE_PARTITION_ENABLE := true
+
 ###########
 #QMAA flags starts
 ###########
@@ -51,54 +98,134 @@ BOARD_AVB_ENABLE := true
 #true means QMAA is enabled for system
 #false means QMAA is disabled for system
 
-TARGET_USES_QMAA := false
+TARGET_USES_QMAA := true
+
+#QMAA flag which is set to incorporate any generic dependencies
+#required for the boot to UI flow in a QMAA enabled target.
+#Set to false when all target level depenencies are met with
+#actual full blown implementations.
+TARGET_USES_QMAA_RECOMMENDED_BOOT_CONFIG := true
+
 #QMAA tech team flag to override global QMAA per tech team
 #true means overriding global QMAA for this tech area
 #false means using global, no override
-TARGET_USES_QMAA_OVERRIDE_RPMB	:= true
+TARGET_USES_QMAA_OVERRIDE_RPMB := true
+TARGET_USES_QMAA_OVERRIDE_GPT  := true
 TARGET_USES_QMAA_OVERRIDE_DISPLAY := true
-TARGET_USES_QMAA_OVERRIDE_AUDIO   := true
+TARGET_USES_QMAA_OVERRIDE_AUDIO   := false
 TARGET_USES_QMAA_OVERRIDE_VIDEO   := true
 TARGET_USES_QMAA_OVERRIDE_CAMERA  := true
 TARGET_USES_QMAA_OVERRIDE_GFX     := true
 TARGET_USES_QMAA_OVERRIDE_WFD     := true
 TARGET_USES_QMAA_OVERRIDE_GPS     := true
-TARGET_USES_QMAA_OVERRIDE_ANDROID_RECOVERY := true
 TARGET_USES_QMAA_OVERRIDE_ANDROID_CORE := true
 TARGET_USES_QMAA_OVERRIDE_WLAN    := true
-TARGET_USES_QMAA_OVERRIDE_DPM  := true
 TARGET_USES_QMAA_OVERRIDE_BLUETOOTH   := true
-TARGET_USES_QMAA_OVERRIDE_FM  := true
-TARGET_USES_QMAA_OVERRIDE_CVP  := true
+TARGET_USES_QMAA_OVERRIDE_FM  := false
+TARGET_USES_QMAA_OVERRIDE_BLUETOOTH_AUDIO := true
+TARGET_USES_QMAA_OVERRIDE_CVP  := false
+TARGET_USES_QMAA_OVERRIDE_SV  := false
 TARGET_USES_QMAA_OVERRIDE_FASTCV  := true
-TARGET_USES_QMAA_OVERRIDE_SCVE  := true
-TARGET_USES_QMAA_OVERRIDE_OPENVX  := true
-TARGET_USES_QMAA_OVERRIDE_DIAG := true
-TARGET_USES_QMAA_OVERRIDE_FTM := true
+TARGET_USES_QMAA_OVERRIDE_SCVE  := false
 TARGET_USES_QMAA_OVERRIDE_DATA := true
 TARGET_USES_QMAA_OVERRIDE_DATA_NET := true
-TARGET_USES_QMAA_OVERRIDE_MSM_BUS_MODULE := true
-TARGET_USES_QMAA_OVERRIDE_KERNEL_TESTS_INTERNAL := true
+TARGET_USES_QMAA_OVERRIDE_DATA_CONNECTIVITY := true
+TARGET_USES_QMAA_OVERRIDE_RIL_DATA := true
+TARGET_USES_QMAA_OVERRIDE_DATA_IPA_AGENT := false
+TARGET_USES_QMAA_OVERRIDE_MSM_BUS_MODULE := false
+TARGET_USES_QMAA_OVERRIDE_DRM    := false
+TARGET_USES_QMAA_OVERRIDE_KMGK := false
 TARGET_USES_QMAA_OVERRIDE_MSMIRQBALANCE := true
-TARGET_USES_QMAA_OVERRIDE_VIBRATOR := true
-TARGET_USES_QMAA_OVERRIDE_DRM     := true
-TARGET_USES_QMAA_OVERRIDE_KMGK := true
-TARGET_USES_QMAA_OVERRIDE_VPP := true
+TARGET_USES_QMAA_OVERRIDE_VPP := false
 TARGET_USES_QMAA_OVERRIDE_GP := true
-TARGET_USES_QMAA_OVERRIDE_SPCOM_UTEST := true
 TARGET_USES_QMAA_OVERRIDE_PERF := true
+TARGET_USES_QMAA_OVERRIDE_SENSORS := true
+TARGET_USES_QMAA_OVERRIDE_SYNX := false
+TARGET_USES_QMAA_OVERRIDE_SECUREMSM_TESTS := false
+TARGET_USES_QMAA_OVERRIDE_SMCINVOKE := false
+TARGET_USES_QMAA_OVERRIDE_SOTER := false
+TARGET_USES_QMAA_OVERRIDE_REMOTE_EFS := false
+TARGET_USES_QMAA_OVERRIDE_USB := true
+TARGET_USES_QMAA_OVERRIDE_DPM := false
+TARGET_USES_QMAA_OVERRIDE_FASTRPC := true
+TARGET_USES_QMAA_OVERRIDE_SPU := false
+TARGET_USES_QMAA_OVERRIDE_UWB := false
+TARGET_USES_QMAA_OVERRIDE_SST_CLIENTS := false
+TARGET_USES_QMAA_OVERRIDE_TIME_SERVICES := false
+TARGET_USES_QMAA_OVERRIDE_VIBRATOR:= false
 
 #Full QMAA HAL List
 QMAA_HAL_LIST := audio video camera display sensors gps
 
+ifeq ($(TARGET_USES_QMAA), true)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.confqmaa=true
+endif
+
 ###########
 #QMAA flags ends
+
+# Enable USB detection in QMAA mode
+ifeq ($(TARGET_USES_QMAA),true)
+ifeq ($(TARGET_USES_QMAA_RECOMMENDED_BOOT_CONFIG),true)
+ifeq ($(TARGET_USES_QMAA_OVERRIDE_USB), false)
+PRODUCT_PACKAGES += init.qti.usb.qmaa.rc
+# Enable USB in adb-only configuration
+PRODUCT_PROPERTY_OVERRIDES += persist.vendor.usb.config=adb
+endif
+endif
+endif
 
 #Suppot to compile recovery without msm headers
 TARGET_HAS_GENERIC_KERNEL_HEADERS := true
 
-SHIPPING_API_LEVEL := 30
-PRODUCT_SHIPPING_API_LEVEL := 30
+CLEAN_UP_JAVA_IN_VENDOR ?= enforcing
+
+JAVA_IN_VENDOR_SOONG_WHITE_LIST :=\
+CuttlefishService\
+pasrservice\
+VendorPrivAppPermissionTest\
+MediaDrmAPITest\
+CastSignAPITest\
+
+JAVA_IN_VENDOR_MAKE_WHITE_LIST :=\
+AEye\
+AON\
+SnapdragonCamera\
+
+SHIPPING_API_LEVEL := 36
+PRODUCT_SHIPPING_API_LEVEL := 36
+
+# Set kernel version and ion flags
+TARGET_KERNEL_VERSION := 5.15
+TARGET_USES_NEW_ION := true
+
+# Disable DLKM generation until build support is available
+TARGET_KERNEL_DLKM_DISABLE := true
+
+# Tech specific flags
+TARGET_KERNEL_DLKM_AUDIO_OVERRIDE := false
+TARGET_KERNEL_DLKM_BT_OVERRIDE := true
+TARGET_KERNEL_DLKM_CAMERA_OVERRIDE := true
+TARGET_KERNEL_DLKM_NFC_OVERRIDE := false
+TARGET_KERNEL_DLKM_ESE_OVERRIDE := false
+TARGET_KERNEL_DLKM_DATA_OVERRIDE := true
+TARGET_KERNEL_DLKM_DISPLAY_OVERRIDE := true
+TARGET_KERNEL_DLKM_MM_DRV_OVERRIDE := true
+TARGET_KERNEL_DLKM_SECURE_MSM_OVERRIDE := true
+TARGET_KERNEL_DLKM_SECUREMSM_QTEE_OVERRIDE := true
+TARGET_KERNEL_DLKM_LIMITS_OVERRIDE := false
+TARGET_KERNEL_DLKM_TOUCH_OVERRIDE := true
+TARGET_KERNEL_DLKM_VIDEO_OVERRIDE := true
+TARGET_KERNEL_DLKM_WLAN_OVERRIDE := true
+TARGET_KERNEL_DLKM_MMRM_OVERRIDE := true
+TARGET_KERNEL_DLKM_DATARMNET_OVERRIDE := true
+TARGET_KERNEL_DLKM_DATARMNETEXT_OVERRIDE := true
+TARGET_KERNEL_DLKM_SYNX_OVERRIDE := false
+TARGET_KERNEL_DLKM_DATAIPA_OVERRIDE := true
+TARGET_KERNEL_DLKM_FASTRPC_OVERRIDE := true
+TARGET_KERNEL_DLKM_EVA_OVERRIDE := false
+TARGET_KERNEL_DLKM_SPU_OVERRIDE := false
 
 #####Dynamic partition Handling
 ###
@@ -106,13 +233,23 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 PRODUCT_BUILD_ODM_IMAGE := true
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += fastbootd
-# Add default implementation of fastboot HAL.
-PRODUCT_PACKAGES += android.hardware.fastboot@1.0-impl-mock
+# Add default implementation of fastboot AIDL.
+PRODUCT_PACKAGES += android.hardware.fastboot-service.example_recovery
 
 ifeq ($(ENABLE_AB),true)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+ifeq ($(SYSTEMEXT_SEPARATE_PARTITION_ENABLE), true)
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.emmc
 else
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_noSysext.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+endif
+else
+ifeq ($(SYSTEMEXT_SEPARATE_PARTITION_ENABLE), true)
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/default/fstab_non_AB.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/emmc/fstab_non_AB.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.emmc
+else
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/fstab_non_AB_noSysext.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+endif
 endif
 BOARD_AVB_VBMETA_SYSTEM := system
 BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
@@ -120,7 +257,9 @@ BOARD_AVB_VBMETA_SYSTEM_ALGORITHM := SHA256_RSA2048
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
-$(call inherit-product, build/make/target/product/gsi_keys.mk)
+ifneq ("$(wildcard device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist)", "")
+PRODUCT_COPY_FILES += device/qcom/$(TARGET_BOARD_PLATFORM)-kernel/vendor_dlkm/system_dlkm.modules.blocklist:$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules/system_dlkm.modules.blocklist
+endif
 
 BOARD_HAVE_BLUETOOTH := false
 BOARD_HAVE_QCOM_FM := false
@@ -129,6 +268,8 @@ BOARD_HAVE_QCOM_FM := false
 PRODUCT_PROPERTY_OVERRIDES += ro.control_privapp_permissions=enforce
 
 TARGET_DEFINES_DALVIK_HEAP := true
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, device/qcom/vendor-common/common64.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
@@ -184,14 +325,6 @@ TARGET_USES_RRO := true
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
 ###########
-# Kernel configurations
-#Enable llvm support for kernel
-KERNEL_LLVM_SUPPORT := true
-#Enable sd-llvm support for kernel
-KERNEL_SD_LLVM_SUPPORT := false
-
-
-###########
 # Target configurations
 
 QCOM_BOARD_PLATFORMS += lahaina
@@ -217,10 +350,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 
 BOARD_FRP_PARTITION_NAME := frp
 
-
-# lights hal
-PRODUCT_PACKAGES += lights.qcom
-
 # Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
@@ -232,9 +361,8 @@ ifeq ($(ENABLE_AB), true)
 PRODUCT_PACKAGES += update_engine \
     update_engine_client \
     update_verifier \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 PRODUCT_HOST_PACKAGES += \
     brillo_update_payload
@@ -253,6 +381,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_HOST_PACKAGES += \
     configstore_xmlparser
 
+ifneq (,$(wildcard $(QCPATH)/vsdk-tools))
+PRODUCT_HOST_PACKAGES += \
+    install_vsdk_py2
+PRODUCT_HOST_PACKAGES += \
+    install_vsdk_py3
+PRODUCT_HOST_PACKAGES += \
+    vsdk-metadata
+endif
+
 # QRTR related packages
 PRODUCT_PACKAGES += qrtr-ns
 PRODUCT_PACKAGES += qrtr-lookup
@@ -260,7 +397,6 @@ PRODUCT_PACKAGES += libqrtr
 
 # diag-router
 TARGET_HAS_DIAG_ROUTER := true
-
 
 # f2fs utilities
 PRODUCT_PACKAGES += \
@@ -278,17 +414,27 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
-# Camera configuration file. Shared by passthrough/binderized camera HAL
-PRODUCT_PACKAGES += camera.device@3.2-impl
-PRODUCT_PACKAGES += camera.device@1.0-impl
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
-# Enable binderized camera HAL
-PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service_64
+# Macro allows Camera module to use new service
+QTI_CAMERA_PROVIDER_SERVICE := 2.7
+QTI_CAMERA_AON_SERVICE := 1.2
 
 DEVICE_FRAMEWORK_MANIFEST_FILE := device/qcom/lahaina/framework_manifest.xml
 
-DEVICE_MANIFEST_SKUS := lahaina
+# Enable compilation of image_generation_tool
+TARGET_USES_IMAGE_GEN_TOOL := true
+
+# QCV allows multiple chipsets to be supported on a single vendor.
+# Add vintf device manifests for chipsets in lahaina QCV family below.
+ifeq ($(TARGET_USES_QMAA), true)
+TARGET_USES_QCV := false
+else
+TARGET_USES_QCV := true
+endif
+
+DEVICE_MANIFEST_SKUS := lahaina shima yupik
 DEVICE_MANIFEST_LAHAINA_FILES := device/qcom/lahaina/manifest_lahaina.xml
+DEVICE_MANIFEST_SHIMA_FILES := device/qcom/lahaina/manifest_shima.xml
+DEVICE_MANIFEST_YUPIK_FILES := device/qcom/lahaina/manifest_yupik.xml
 
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 
@@ -322,9 +468,13 @@ DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
 
 PRODUCT_PACKAGES += $(AUDIO_DLKM)
 
+#Key derivation in vts kernel encryption tests use legacykdf
+PRODUCT_VENDOR_PROPERTIES += ro.crypto.hw_wrapped_keys.kdf=legacykdf
+
 # Kernel modules install path
 KERNEL_MODULES_INSTALL := dlkm
 KERNEL_MODULES_OUT := out/target/product/$(PRODUCT_NAME)/$(KERNEL_MODULES_INSTALL)/lib/modules
+
 
 
 USE_LIB_PROCESS_GROUP := true
@@ -333,26 +483,27 @@ USE_LIB_PROCESS_GROUP := true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+# Dumpstate Feature Flag
+PRODUCT_ENABLE_DUMPSTATE_SUPPORT := true
 
 #Enable full treble flag
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
-BOARD_SYSTEMSDK_VERSIONS := 30
-BOARD_VNDK_VERSION := current
+
+ifeq (true,$(BUILDING_WITH_VSDK))
+    ALLOW_MISSING_DEPENDENCIES := true
+    TARGET_SKIP_CURRENT_VNDK := true
+    -include vendor/qcom/vsdk_snapshots_config/config.mk
+else
+    RECOVERY_SNAPSHOT_VERSION := current
+    RAMDISK_SNAPSHOT_VERSION := current
+endif
+
+$(warning "BOARD_VNDK_VERSION = $(BOARD_VNDK_VERSION), RECOVERY_SNAPSHOT_VERSION=$(RECOVERY_SNAPSHOT_VERSION), RAMDISK_SNAPSHOT_VERSION=$(RAMDISK_SNAPSHOT_VERSION)")
+
 TARGET_MOUNT_POINTS_SYMLINKS := false
 
-# FaceAuth feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.biometrics.face.xml \
-
-# Fingerprint feature
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-
-# system prop for enabling QFS (QTI Fingerprint Solution)
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qfp=true
 #target specific runtime prop for qspm
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qspm.enable=true
@@ -374,34 +525,69 @@ endif
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
+ifeq ($(TARGET_USES_QMAA), true)
+ifneq ($(TARGET_USES_QMAA_OVERRIDE_ANDROID_CORE),true)
+PRODUCT_ENABLE_QESDK := false
+else
+PRODUCT_ENABLE_QESDK := true
+endif
+else
+PRODUCT_ENABLE_QESDK := true
+endif
 
 # Vendor property to enable advanced network scanning
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.enableadvancedscan=true
 
-PRODUCT_COPY_FILES += \
-    device/qcom/lahaina/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+# Enable Fuse Passthrough
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.fuse.passthrough.enable=true
 
 # ODM ueventd.rc
 # - only for use with VM support right now
 ifeq ($(TARGET_ENABLE_VM_SUPPORT),true)
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/ueventd-odm.rc:$(TARGET_COPY_OUT_ODM)/ueventd.rc
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/ueventd-odm.rc:$(TARGET_COPY_OUT_ODM)/etc/ueventd.rc
 PRODUCT_PACKAGES += vmmgr vmmgr.rc vmmgr.conf
 endif
 
-PRODUCT_PACKAGES += android.hardware.lights-service.qti
+#qspa service
+PRODUCT_PACKAGES += vendor.qti.qspa-service qspa-testclient
 
-PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+PRODUCT_PACKAGES += qspa_vendor.rc
 
-PRODUCT_ENABLE_QESDK := true
+##Armv9-Tests##
+PRODUCT_PACKAGES_DEBUG += bti_test_prebuilt \
+                          pac_test \
+                          mte_tests \
+                          dynamic_memcpy_prebuilt
+##Armv9-Tests##
+
+# TODO(b/330696629) remove this once device can drop HIDL.
+# This adds hwservicemanager and the allocator service to the device.
+PRODUCT_PACKAGES += \
+    hwservicemanager \
+    android.hidl.allocator@1.0-service
+
+# Mediaserver 64 Bit enable
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.mediaserver.64b.enable=true
+
+# VM shutdown feature enablement
+PRODUCT_VENDOR_PROPERTIES += \
+     ro.vendor.vm.ssr.enable=false
+
+# Enable support for APEX updates
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+ifneq ($(TARGET_USES_QMAA), true)
+ifeq ($(TARGET_USES_QMAA_OVERRIDE_ANDROID_CORE),true)
+endif
+endif
 
 ###################################################################################
 # This is the End of target.mk file.
 # Now, Pickup other split product.mk files:
 ###################################################################################
-# TODO: Relocate the system product.mk files pickup into qssi lunch, once it is up.
-$(foreach sdefs, $(sort $(wildcard vendor/qcom/defs/product-defs/system/*.mk)), \
-    $(call inherit-product, $(sdefs)))
 $(foreach vdefs, $(sort $(wildcard vendor/qcom/defs/product-defs/vendor/*.mk)), \
     $(call inherit-product, $(vdefs)))
 ###################################################################################
+
