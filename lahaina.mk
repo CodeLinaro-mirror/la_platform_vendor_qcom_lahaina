f4f5967a1d51a74268e1f13621401a71705f9043
Sudheer Kanchumurthi <c_skanch@qti.qualcomm.com>
2026-02-06 09:16:24 +0530

    lahaina: updating PRODUCT_MAX_PAGE_SIZE_SUPPORTED to 4k
    Since lahaina using qssi which is supporting 4k page size only, changing from vendor side as well
    
    Change-Id: I7687b7655a6d02556b5836a7f7a3b55f99cadb14

--- lahaina.mk
+++ lahaina.mk
@@ -42,8 +42,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Set GRF/Vendor freeze properties
 BOARD_SHIPPING_API_LEVEL := 202504
 
-#Align ELF segment of binaries to 16k
-PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 16384
+#Align ELF segment of binaries to 4k
+PRODUCT_MAX_PAGE_SIZE_SUPPORTED := 4096
 
 #Enable AOSP to be page size agnostic
 PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
