7bb77ca143550fdad459f091fdaf55bcaae82b46
Kartheek Makkena <kmakkena@qti.qualcomm.com>
2026-07-01 10:39:15 +0530

    Lahaina: Disable SMMU_PROXY
    With CSF 2.0, this target no longer requires an SMMU proxy.
    
    Change-Id: Ibce4b43884a8d286f0308278574980be0ab16b61

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,7 +139,7 @@ BOARD_AVB_BOOT_ROLLBACK_INDEX_LOCATION := 3
 
 TARGET_USES_ION := true
 TARGET_USES_NEW_ION_API := true
-TARGET_USES_SMMU_PROXY := true
+TARGET_USES_SMMU_PROXY := false
 
 BOARD_KERNEL_CMDLINE := video=vfb:640x400,bpp=32,memsize=3072000 loglevel=8
 BOARD_BOOTCONFIG := androidboot.hardware=qcom androidboot.memcg=1 androidboot.usbcontroller=a600000.dwc3 androidboot.load_modules_parallel=true
